2a3631599504d0e836c3645a0b4065d51255e0a1
crives <car.rives@gmail.com>
2020-08-13 19:36:04 -0400

    making registration data values

--- src/main/resources/SQL_Scripts/Registration.sql
+++ src/main/resources/SQL_Scripts/Registration.sql
@@ -1,3 +1,10 @@
 SELECT * FROM student_db.registration;
 
-insert into student_db.registration (registration_id, course_id, student_id) values ( );
+insert into student_db.registration (registration_id, course_id, student_id) values (1, "MTH200", 1);
+insert into student_db.registration (registration_id, course_id, student_id) values (2, "SOC300", 2);
+insert into student_db.registration (registration_id, course_id, student_id) values (3, "BMX100", 3);
+insert into student_db.registration (registration_id, course_id, student_id) values (4, "PSY220", 4);
+insert into student_db.registration (registration_id, course_id, student_id) values (5, "ATB100", 5);
+insert into student_db.registration (registration_id, course_id, student_id) values (6, "AMS250", 6);
+insert into student_db.registration (registration_id, course_id, student_id) values (7, "SQG300", 7);
+insert into student_db.registration (registration_id, course_id, student_id) values (8, "ENG230", 8);
