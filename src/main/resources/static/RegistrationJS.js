ddad52b4a2d212a06e67cd560ab79f8adfb9a56d
crives <car.rives@gmail.com>
2020-08-15 00:19:44 -0400

    Update RegistrationJS.js
    Changed formatting

--- src/main/resources/static/RegistrationJS.js
+++ src/main/resources/static/RegistrationJS.js
@@ -3,15 +3,11 @@ function initialize(){
     getAllCourses("/api/courses/")
     generateTables();
 }
-
 function getAllCourses(url) {
-
     //make initial api call to get Student list
     var xhttpList = new XMLHttpRequest();
-
     // Read JSON - and put in storage
     xhttpList.onreadystatechange = function () {
-
         if (this.readyState == 4 && this.status == 200) {
             localStorage.setItem("allCourses", xhttpList.responseText);        
         }
@@ -19,16 +15,12 @@ function getAllCourses(url) {
     xhttpList.open("GET", url, true);
     xhttpList.send();
     console.log("Registration List stored");
-
 }
 function getRegistrations(url) {
-
     //make initial api call to get Student list
     var xhttpList = new XMLHttpRequest();
-
     // Read JSON - and put in storage
     xhttpList.onreadystatechange = function () {
-
         if (this.readyState == 4 && this.status == 200) {
             localStorage.setItem("allRegistrations", xhttpList.responseText);        
         }
@@ -36,30 +28,22 @@ function getRegistrations(url) {
     xhttpList.open("GET", url, true);
     xhttpList.send();
     console.log("Registration List stored");
-
 }
-
 function generateTables() {
     getStudentCourses();
-	//get tables 
+    //get tables 
     var studentClasses = document.getElementById("studentClasses");
     var courseCatalog = document.getElementById("courseCatalog");
-
-	//generateStudentClasses(studentClasses);
-	//generateCourseCatalog(studentClasses);
+    //generateStudentClasses(studentClasses);
+    //generateCourseCatalog(studentClasses);
     generateStudentClasses(studentClasses);
-	generateCourseCatalog(courseCatalog);
-	
+    generateCourseCatalog(courseCatalog);
 }
-
 function getStudentCourses(){
     url = "api/registration/2"
-
     var xhttpList = new XMLHttpRequest();
-
     // Read JSON - and put in storage
     xhttpList.onreadystatechange = function () {
-
         if (this.readyState == 4 && this.status == 200) {
             localStorage.setItem("studentRegistrations", xhttpList.responseText);  
         }
@@ -67,15 +51,11 @@ function getStudentCourses(){
     xhttpList.open("GET", url, true);
     xhttpList.send();
     console.log("Student Registrations List stored");
-
 }
-
 function getOneRegistration(url) {
-
     //Get an individual pokemon from the nested endpoint
     var xhttpList = new XMLHttpRequest();
     xhttpList.open("GET", url, false);
-
     xhttpList.onreadystatechange = function() {
     if(this.readyState == 4 && this.status == 200) {
         console.log("loaded into registration url");
@@ -83,109 +63,96 @@ function getOneRegistration(url) {
         }
     };
     xhttpList.send();
-        
 }
-
 function generateStudentClasses(table){
 
     console.log(localStorage.getItem("studentRegistrations"));
     var studentClasses = JSON.parse(localStorage.getItem("studentRegistrations"));
     generateStudentClassesHead(table,studentClasses);
     console.log(studentClasses)
-    for(var i = 0;i<studentClasses.length;i++){
-    
-    	var row = table.insertRow(); 
-    	var studentClass = studentClasses[i];
-   		for(element in studentClasses[i]){
-   			
-   			console.log(element);
-   			var cell = row.insertCell();
-   			cell.style.border = "1px solid black";
-   			var content = document.createTextNode(studentClass[element]);
-   			if(element == "description"){
-   				//var button = document.createElement("button");
-   				cell.innerHTML = '<button data-toggle="collapse" data-target="#description">Description</button>';
-   				var div = document.createElement("div");
-   				div.id = "description";
-   				div.className = "collapse";
-   				div.appendChild(content);
-   				cell.appendChild(div)
-   				row.appendChild(cell);
-   				continue;
-   			}
-   			cell.appendChild(content);
-    		row.appendChild(cell);
-    	}
-    
-    	
-    }
-    
-}
 
+    for(var i = 0; i < studentClasses.length;i++){
+
+        var row = table.insertRow(); 
+        var studentClass = studentClasses[i];
+
+        for(element in studentClasses[i]){
+
+            console.log(element);
+            var cell = row.insertCell();
+            cell.style.border = "1px solid black";
+            var content = document.createTextNode(studentClass[element]);
+            if(element == "description"){
+                //var button = document.createElement("button");
+                cell.innerHTML = '<button data-toggle="collapse" data-target="#description">Description</button>';
+                var div = document.createElement("div");
+                div.id = "description";
+                div.className = "collapse";
+                div.appendChild(content);
+                cell.appendChild(div)
+                row.appendChild(cell);
+                continue;
+            }
+            cell.appendChild(content);
+            row.appendChild(cell);
+        }
+    }
+}
 function generateStudentClassesHead(table, studentClasses){
+
     var head = table.createTHead();
     var row = head.insertRow();
-    
     for(element in studentClasses){
-    	
         var th = document.createElement("th");
         var text = document.createTextNode(studentClasses[element]);
         th.style.border = "2px solid black";
-        
         th.appendChild(text);
         row.appendChild(th);
     }
-
 }
-
 function generateCourseCatalog(table){
-	
-    
+
     console.log(localStorage.getItem("allCourses"));
     var courses = JSON.parse(localStorage.getItem("allCourses"));
-    generateCourseHead(table,courses[0]);
+    generateCourseHead(table,courses);
     console.log(courses)
-    for(var i = 0;i<courses.length;i++){
-    
-    	var row = table.insertRow(); 
-    	var course = courses[i];
-   		for(element in courses[i]){
-   			
-   			console.log(element);
-   			var cell = row.insertCell();
-   			cell.style.border = "1px solid black";
-   			var content = document.createTextNode(course[element]);
-   			if(element == "description"){
-   				//var button = document.createElement("button");
-   				cell.innerHTML = '<button data-toggle="collapse" data-target="#description">Description</button>';
-   				var div = document.createElement("div");
-   				div.id = "description";
-   				div.className = "collapse";
-   				div.appendChild(content);
-   				cell.appendChild(div)
-   				row.appendChild(cell);
-   				continue;
-   			}
-   			cell.appendChild(content);
-    		row.appendChild(cell);
-    	}
-    
-    	
-    }
-    
-}
 
+    for(var i = 0; i < courses.length; i++){
+        var row = table.insertRow(); 
+        var course = courses[i];
+
+        for(element in courses[i]){
+
+            console.log(element);
+            var cell = row.insertCell();
+            cell.style.border = "1px solid black";
+            var content = document.createTextNode(course[element]);
+            if(element == "description"){
+                //var button = document.createElement("button");
+                cell.innerHTML = '<button data-toggle="collapse" data-target="#description">Description</button>';
+                var div = document.createElement("div");
+                div.id = "description";
+                div.className = "collapse";
+                div.appendChild(content);
+                cell.appendChild(div)
+                row.appendChild(cell);
+                continue;
+            }
+            cell.appendChild(content);
+            row.appendChild(cell);
+        }
+    }
+}
 function generateCourseHead(table,courses){
-	
-	var head = table.createTHead();
+
+    var head = table.createTHead();
     var row = head.insertRow();
-    
-    for(element in courses[0]){
-    	
-        var th = document.createElement("th");
-        var text = document.createTextNode(course[element]);
-        th.style.border = "2px solid black";
+
+    for(element in courses){
         
+        var th = document.createElement("th");
+        var text = document.createTextNode(courses[element]);
+        th.style.border = "2px solid black";
         th.appendChild(text);
         row.appendChild(th);
     }
